f440ef0db29a0999d68f343df4f42858da541ef7
John villlamil <jvillamil@geinsys.com>
2022-03-17 09:50:58 -0500

    second challenge, n params

--- sum.js
+++ sum.js
@@ -2,14 +2,6 @@ function sum(a) {
     if(typeof a == "function"){
         return a(`prints: -> 0`);
     }
-    return b => b === undefined ? a : sum(((typeof b !="number")?b(`prints: -> ${a}`):a+b));
+    return b => typeof b === 'function' ? b(`prints: -> ${a}`) : sum(a+b);
 }
-
- 
-
-
-sum(result=>{console.log("-> ",result)});
-sum(1)(result=>console.log("-> ",result));
-// sum(1)(2)(result=>console.log("-> ",result));
-// sum(2)(3)(6)(result=>{console.log("-> ",result)});
 module.exports = sum;
--- sum.test.js
+++ sum.test.js
@@ -1,17 +1,17 @@
 const sum = require('./sum');
 
-// test('zero', () => {
-//   expect(sum(result=>{return "-> "+result})).toBe("-> prints: -> 0");
-// });
+test('zero', () => {
+  expect(sum(result=>{return "-> "+result})).toBe("-> prints: -> 0");
+});
 
 test('one', () => {
   expect(sum(1)(result=>{return "-> "+result})).toBe("-> prints: -> 1");
 });
 
-// test('two', () => {
-//   expect(sum(1)(2)(result=>{return "-> "+result})).toBe("-> prints: -> 3");
-// });
+test('two', () => {
+  expect(sum(1)(2)(result=>{return "-> "+result})).toBe("-> prints: -> 3");
+});
 
-// test('three', () => {
-//   expect(sum(1)(2)(4)(result=>{return "-> "+result})).toBe("-> prints: -> 7");
-// });
+test('three', () => {
+  expect(sum(1)(2)(4)(result=>{return "-> "+result})).toBe("-> prints: -> 7");
+});
